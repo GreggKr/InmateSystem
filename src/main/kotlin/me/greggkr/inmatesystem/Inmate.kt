6e8ac51afec5cfbc50056ddbfa07e3d5b07a666a
Gregg <greggkrzykowski@gmail.com>
2018-02-24 14:13:02 -0600

    woah that's way cleaner

--- src/main/kotlin/me/greggkr/inmatesystem/Inmate.kt
+++ src/main/kotlin/me/greggkr/inmatesystem/Inmate.kt
@@ -1,7 +1,12 @@
 package me.greggkr.inmatesystem
 
 // TODO: convert country into an enum?
-data class Inmate(private val number: Long, private val firstName: String, private val lastName: String, private val country: String, private val crimes: List<Crime>, private val prison: Prison? = null) {
+data class Inmate(private val number: Long,
+                  private val firstName: String,
+                  private val lastName: String,
+                  private val country: String,
+                  private val crimes: List<Crime>,
+                  private val prison: Prison? = null) {
     fun getTimeInPrison(): Long {
         var time: Long = 0
         crimes.forEach { time += it.time }
