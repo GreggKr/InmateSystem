268d9916cb2e6d66ddcff9e7455429470fdba36d
Gregg <greggkrzykowski@gmail.com>
2018-02-24 14:00:28 -0600

    data class, rm builder

--- src/main/kotlin/me/greggkr/inmatesystem/Inmate.kt
+++ src/main/kotlin/me/greggkr/inmatesystem/Inmate.kt
@@ -1,16 +1,9 @@
 package me.greggkr.inmatesystem
 
-import java.util.*
-
 // TODO: convert country into an enum?
-class Inmate(private val number: Long, private val firstName: String, private val lastName: String, private val country: String, private val crimes: List<Crime>, private val prison: Prison? = null) {
-    override fun toString(): String {
-        return "Inmate[num=$number, name=${"$firstName $lastName"}, country=$country, inJail=${prison != null}, crimes=$crimes, prison=$prison]"
-    }
-
+data class Inmate(private val number: Long, private val firstName: String, private val lastName: String, private val country: String, private val crimes: List<Crime>, private val prison: Prison? = null) {
     fun getTimeInPrison(): Long {
         var time: Long = 0
-
         crimes.forEach { time += it.time }
 
         return time
@@ -19,30 +12,5 @@ class Inmate(private val number: Long, private val firstName: String, private va
     enum class SecurityLevel(val level: Int) {
         MIN(0),
         MAX(3);
-    }
-
-    class Builder {
-        private var number: Long = Long.MIN_VALUE
-        private var firstName: String = ""
-        private var lastName: String = ""
-        private var country: String = ""
-        private var crimes: List<Crime> = ArrayList()
-        private var prison: Prison? = null
-
-        fun setNumber(number: Long) = also { this.number = number }
-
-        fun setFirstName(firstName: String) = also { this.firstName = firstName }
-
-        fun setLastName(lastName: String) = also { this.lastName = lastName }
-
-        fun setCountry(country: String) = also { this.country = country }
-
-        fun setPrison(prison: Prison) = also { this.prison = prison }
-
-        fun setCrimes(vararg crimes: Crime) = also { this.crimes = crimes.toList() }
-
-        fun build(): Inmate {
-            return Inmate(number, firstName, lastName, country, crimes, prison)
-        }
     }
 }
